3494e3d670df6d0f4d7c1865835ca37b3e9ddf31
Matheus Germano <matheuslucel@gmail.com>
2022-10-31 15:05:04 -0300

    feat/germinare/general props settings

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,10 @@
-
 import './style.scss'
+import { DetailedHTMLProps, ButtonHTMLAttributes} from 'react'
 
 export type ButtonProps = {
     content: string
     variant: "button-filled" | "button-filled-tech" | "button-outlined" 
-} 
+} & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 
 export default function Button({ content, variant, ...props }: ButtonProps) {
 
