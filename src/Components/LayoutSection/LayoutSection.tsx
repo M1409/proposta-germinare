20ab8bf0f2b6813ebd9b4fe67ef7fc52703d4c6b
Matheus Germano <82274410+M1409@users.noreply.github.com>
2022-11-07 17:09:14 -0300

    feat(015): adding layout section component (#2)

---
+++ src/Components/LayoutSection/LayoutSection.tsx
@@ -0,0 +1,21 @@
+import './style.scss'
+import { DetailedHTMLProps, ButtonHTMLAttributes } from 'react'
+
+export type LayoutSectionProps = {
+    src: string
+    height?: string
+    color?: string
+} &  DetailedHTMLProps<ButtonHTMLAttributes<HTMLDivElement>, HTMLDivElement>
+
+export function LayoutSection({children, src, color, height, ...props}:LayoutSectionProps){
+
+    return (
+        <div className='layout-section' style = {{backgroundImage: ` url(${src})`,height: height}} {...props}>
+            <span style = {{backgroundColor: color}}>
+                <div>
+                    {children}
+                </div>
+            </span>
+        </div>
+    )
+}
---
+++ src/declaration.d.ts
@@ -0,0 +1,4 @@
+// this file is essencial for import images in this project, because of typescript. Font: https://stackoverflow.com/questions/52759220/importing-images-in-typescript-react-cannot-find-module
+//for more extensions, please just edit this file.
+declare module '*.jpg';
+declare module '*.png';
