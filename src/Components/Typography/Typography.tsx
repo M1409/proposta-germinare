33c314d182b455227b7a052de1dfe8e5f0589681
lucas jesus <lucas.jesus@germinare.org.br>
2022-11-07 16:19:35 -0300

    implementing color to thypography

--- src/Components/Typography/Typography.tsx
+++ src/Components/Typography/Typography.tsx
@@ -2,11 +2,13 @@ import './style.scss'
 
 export type TypographyProps = {
     variant?: 'typography-paragraph' | 'typography-subtitle' | 'typography-title'
+    color?: 'blue' | 'white'
 }   & React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>
 
 
-export function Typography({children,variant,...props}:TypographyProps){
+export function Typography({children,variant, color, ...props}:TypographyProps){
+    
     return (
-        <p {...props} className = {variant}> {children} </p>
+        <p {...props} className = {`${variant} ${color}`}> {children} </p>
     )
 }
