e3d915804dc4454d90c1185dd7440e35c0bde843
Matheus Germano <matheuslucel@gmail.com>
2022-11-07 14:36:03 -0300

    feat/germinare/merge settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { Typography } from './Components/Typography/index';
 
 
 function App() {
   return (
     <div className="App">
-      <Typography variant='typography-title'> dsadasa </Typography>
+      
     </div>
   );
 }
