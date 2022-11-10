e6af2779d539fa70cc5c38ed3fff83dc8c53bd58
Matheus Germano <matheuslucel@gmail.com>
2022-11-10 17:22:15 -0300

    feat/germinare/welcomehome start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { WelcomeHome } from './Components/WelcomeBanner';
 
 
 function App() {
   return (
     <div className="App">
+      <WelcomeHome/>
 
     </div>
   );
