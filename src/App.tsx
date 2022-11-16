df6c37c7e31a0af4657d094f002e8ef943bd40d4
Matheus Germano <matheuslucel@gmail.com>
2022-11-16 19:58:08 -0300

    feat/germinare/ responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import Home from "./Pages/Home/Home";
-function App() {
+import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-dom";
+import { WelcomeBanner } from "./Components/WelcomeBanner";
+import img from './img.png'
+
+
+export default function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route path="/about" element = {<WelcomeBanner bannerHeight="360px" crumbColor="blue" bannerSrc={img} titleWidth = '900px'/>} />
+        </Switch>
+      </Router>
     </div>
 
   );
