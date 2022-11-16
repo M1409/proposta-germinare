b71f1dbe96c211bb56cad536000a41d22e944250
Lucas <lucas.lsj1711@gmail.com>
2022-11-16 15:36:38 -0300

    fixing responsivity and another feedbacks

---
+++ src/Assets/Images/index.ts
@@ -0,0 +1,4 @@
+export { default as iinsta } from './iinsta.svg';
+export { default as itk } from './itk.svg';
+export { default as ifb } from './ifb.svg';
+export { default as iyt } from './iyt.svg';
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,28 +1,25 @@
 import styles from './Footer.module.scss'
 import { Typography } from '../Typography'
-import ifb from '../../Assets/Images/ifb.svg'
-import iinsta from '../../Assets/Images/iinsta.svg'
-import itk from '../../Assets/Images/itk.svg'
-import iyt from '../../Assets/Images/iyt.svg'
+import { ifb, iinsta, itk, iyt } from '../../Assets/Images/index'
 export default function Footer() {
     return (
         <footer className={styles.footer}>
             <div className={styles.mainContainer}>
                 <div>
-                    <p className={styles.title} id={styles.nameTitle}><span>Instituto Germinare</span> <span id={styles.titleLine}>-</span> <span>Escola de Negócios</span></p>
-                    <p className={styles.contact}>
+                    <h6 className={styles.title} id={styles.nameTitle}><span>Instituto Germinare</span> <span id={styles.titleLine}>-</span> <span>Escola de Negócios</span></h6>
+                    <h6 className={styles.contact}>
                         Rua Irineu José Bordon, 335<br></br> São Paulo, SP - Brasil - 05120-060<br></br>
                         (11) 3623-6000
-                    </p>
+                    </h6>
                 </div>
                 <div className={styles.socialContainer}>
-                    <p className={styles.title}>Redes Sociais</p>
-                    <div className={styles.icons}>
-                        <a target='_blank' href="https://www.facebook.com/germinareoficial"><img src={ifb} alt='Facebook' /></a>
-                        <a target='_blank' href="https://www.instagram.com/escolagerminare/"><img src={iinsta} alt="Instagram" /></a>
-                        <a target='_blank' href="https://www.tiktok.com/@escolagerminare?_t=8XH9B4CndUk&_r=1"><img src={itk} alt="Tik Tok" /></a>
-                        <a target='_blank' href="https://www.youtube.com/channel/UCtfT0wIFhW2jyCMR0ZtBuzg"><img src={iyt} alt="Youtube" /></a>
-                    </div>
+                    <h6 className={styles.title}>Redes Sociais</h6>
+                    <ul className={styles.icons}>
+                        <li><a target='_blank' href="https://www.facebook.com/germinareoficial"><img src={ifb} alt='Facebook' /></a></li>
+                        <li><a target='_blank' href="https://www.instagram.com/escolagerminare/"><img src={iinsta} alt="Instagram" /></a></li>
+                        <li><a target='_blank' href="https://www.tiktok.com/@escolagerminare?_t=8XH9B4CndUk&_r=1"><img src={itk} alt="Tik Tok" /></a></li>
+                        <li><a target='_blank' href="https://www.youtube.com/channel/UCtfT0wIFhW2jyCMR0ZtBuzg"><img src={iyt} alt="Youtube" /></a></li>
+                    </ul>
                 </div>
             </div>
             <div className={styles.posFooter}>
