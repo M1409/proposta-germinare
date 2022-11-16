4df9fd1c27b406bf37f4803e2ded1584e685e3ef
Matheus Germano <matheuslucel@gmail.com>
2022-11-16 18:22:07 -0300

    feat/germinare/card style

--- src/Components/WhiteCard/WhiteCard.tsx
+++ src/Components/WhiteCard/WhiteCard.tsx
@@ -1,11 +1,12 @@
 import { AiOutlineEye } from "react-icons/ai";
 import { Typography } from "../Typography";
+import styles from './WhiteCard.module.scss'
 
 export function WhiteCard(){
     return(
-        <div>
-            <AiOutlineEye/>
-            <Typography color="blue" variant="typography-paragraph" >
+        <div className={styles.WhiteCard}>
+            <AiOutlineEye className={styles.WhiteCardIcon}/>
+            <Typography color="blue" variant="typography-paragraph" style = {{fontWeight: 700}}>
                 Foco no Detalhe
             </Typography>
         </div>
