5db228deb024dca940ccdcc3ca0f37d346b323c5
Lucas <lucas.lsj1711@gmail.com>
2022-11-17 13:40:26 -0300

    fixing develop with updates

--- src/Pages/Admission/Components/CareerSection/CareerSection.tsx
+++ src/Pages/Admission/Components/CareerSection/CareerSection.tsx
@@ -6,7 +6,7 @@ export default function CareerSection() {
         <div>
             <section className={styles.mainSection}>
                 <div style={{ backgroundImage: `url(${mainbg})` }} className={styles.titleContainer}>
-                    <Typography variant='typography-subtitle' color='white'>Está na hora de escolher a<br></br>
+                    <Typography variant='TypographySubtitle' color='white'>Está na hora de escolher a<br></br>
                         <span style={{ color: '#407BFF' }}>SUA CARREIRA</span></Typography>
                     <div className={styles.backgroundShadow}></div>
                 </div>
@@ -14,14 +14,14 @@ export default function CareerSection() {
                 <div className={styles.careerContainer}>
                     <div>
                         <h3>Gestor de Negócios</h3>
-                        <Typography variant='typography-paragraph' >
+                        <Typography variant='TypographyParagraph' >
                             Formar, de maneira efetiva, jovens administradores de empresa que tenham total aderência aos nossos valores, alta capacidade de liderança, aguçado tino comercial e competência para produzir resultados superlativos.
                         </Typography>
                         <img src={chevron} alt="Seta indicativa para acessar o conteúdo" />
                     </div>
                     <div>
                         <h3>Programador</h3>
-                        <Typography variant='typography-paragraph' >
+                        <Typography variant='TypographyParagraph' >
                             Formar, com excelência, jovens Especialistas em Tecnologias Digitais que tenham total aderência aos nossos valores, alta capacidade crítica, analítica e cognitiva, com competências para desenvolver soluções tecnológicas inovadoras a fim de produzir resultados superlativos.
                         </Typography>
                         <img src={chevron} alt="Seta indicativa para acessar o conteúdo" />
