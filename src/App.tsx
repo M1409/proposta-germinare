72ad55e58d32e595078c719e47fd3c0df30cff92
Matheus Germano <matheuslucel@gmail.com>
2022-11-17 13:43:39 -0300

    feat/germinare/whitecard props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { WhiteCard } from "./Components/WhiteCard";
+import { AiOutlineEye } from "react-icons/ai";
 
 export default function App() {
   return (
     <div className="App">
-      <WhiteCard/>
-    </div>
+        <WhiteCard content="Pessoa Certa no Lugar Certo" width="170px"> <AiOutlineEye/></WhiteCard>
+      </div>
   );
 }
--- src/Components/WhiteCard/WhiteCard.tsx
+++ src/Components/WhiteCard/WhiteCard.tsx
@@ -1,13 +1,20 @@
-import { AiOutlineEye } from "react-icons/ai";
+
 import { Typography } from "../Typography";
 import styles from './WhiteCard.module.scss'
 
-export function WhiteCard(){
+export type WhiteCardProps = {
+    content:string
+    width?:string
+} & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+
+export function WhiteCard({content,width,children,...props}:WhiteCardProps){
     return(
-        <div className={styles.WhiteCard}>
-            <AiOutlineEye className={styles.WhiteCardIcon}/>
-            <Typography color="blue" variant="TypographyParagraph" className={styles.WhiteCardText}>
-                Foco no Detalhe
+        <div className={styles.WhiteCard} {...props}>
+            <div className={styles.WhiteCardIcon}>
+                {children}
+            </div>
+            <Typography color="#2E3092" variant="TypographyParagraph" className={styles.WhiteCardText} width = {width}>
+                {content}
             </Typography>
         </div>
     )
