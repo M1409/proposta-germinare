3420de21dfa8bda6347da4942d87146aac6e2942
Lucas <lucas.lsj1711@gmail.com>
2022-11-18 13:59:34 -0300

    fixing develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import {Routes, BrowserRouter as Router, Route} from "react-router-dom";
+import {Routes, HashRouter as Router, Route} from "react-router-dom";
 import { Governance } from "./Pages/Governance";
+import { GeneralNavBar } from "./Components";
+import { Footer } from "./Components";
+import Admission from "./Pages/Admission/Admission";
+import Home from "./Pages/Home/Home";
 export default function App() {
   return (
     <div className="App">
+      <GeneralNavBar></GeneralNavBar>
       <Router>
         <Routes>
-          <Route  path='/' element={<Governance />} />
+          <Route  path='/' element={<Home />} />
+          <Route  path='/home/admissão' element={<Admission />} />
+          <Route  path='/home/governança' element={<Governance />} />
         </Routes>
       </Router>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Components/Typography/Typography.tsx
+++ src/Components/Typography/Typography.tsx
@@ -2,12 +2,12 @@ import './style.scss'
 
 export type TypographyProps = {
     variant?: 'TypographyParagraph' | 'TypographySubtitle' | 'TypographyTitle'
-    color?: '#2E3092' | 'white'
+    color?: '#2E3092' | 'white'  | 'black'
     width?: string
 }   & React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>
 
 
-export function Typography({children,variant, color='#2E3092', width, ...props}:TypographyProps){
+export function Typography({children,variant, color='black', width, ...props}:TypographyProps){
     
     return (
         <p  className = {variant} style = {{color: color, width: width}} {...props}> {children} </p>
