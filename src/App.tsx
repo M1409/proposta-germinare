33f4ff6d194773daf8b50cf8a0f8f8ee8784b545
Lucas <lucas.lsj1711@gmail.com>
2022-11-23 17:58:22 -0300

    starting development

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,60 @@
-import {Routes, HashRouter as Router, Route} from "react-router-dom";
-import { Governance } from "./Pages/Governance";
-import { GeneralNavBar } from "./Components";
-import { Footer } from "./Components";
-import Admission from "./Pages/Admission/Admission";
-import Home from "./Pages/Home/Home";
+import { useState, useEffect, lazy } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+const Governance = lazy(()=> import("./Pages/Governance").then(({ Governance }) => ({ default: Governance })))
+const GeneralNavBar = lazy(()=> import("./Components").then(({ GeneralNavBar }) => ({ default: GeneralNavBar })))
+const Footer = lazy(()=> import("./Components").then(({ Footer }) => ({ default: Footer })))
+const Admission = lazy(() => {
+  return Promise.all([
+    import("./Pages/Admission").then(({ Admission }) => ({ default: Admission })),
+    new Promise(resolve => setTimeout(resolve, 6000))
+  ])
+  .then(([moduleExports]) => moduleExports);
+});
+const VirtualTour = lazy(()=> import("./Pages/VirtualTour").then(({ VirtualTour }) => ({ default: VirtualTour })))
+const Home = lazy(() => {
+  return Promise.all([
+    import("./Pages/Home"),
+    new Promise(resolve => setTimeout(resolve, 6000))
+  ])
+  .then(([moduleExports]) => moduleExports);
+});
 export default function App() {
+  const [tour, setTour] = useState(false);
+  const location = useLocation()
+  useEffect(()=>{
+    if(location.pathname == '/tour'){
+      setTour(true)
+    } else{
+      setTour(false)
+    }
+  }, [location])
   return (
     <div className="App">
-      <GeneralNavBar></GeneralNavBar>
-      <Router>
-        <Routes>
-          <Route  path='/' element={<Home />} />
-          <Route  path='/home/admissão' element={<Admission />} />
-          <Route  path='/home/governança' element={<Governance />} />
-        </Routes>
-      </Router>
-      <Footer></Footer>
-    </div>
+      {!tour ? (
+          <>
+          <GeneralNavBar></GeneralNavBar>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home/admissão" element={<Admission />} />
+              <Route path="/home/governança" element={<Governance />} />
+              <Route
+                path="/tour"
+                element={<VirtualTour />}
+              />
+            </Routes>
+          <Footer></Footer>
+          </>
+      ) : (
+            <>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/tour"
+                element={<VirtualTour />}
+              />
+            </Routes>
+            </>
+      )}
+      </div>
   );
 }
