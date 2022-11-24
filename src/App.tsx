6cba38d03483a066eaa73ecaf4056a0a016e164b
AlexandroMinobe <alexkatsuharu@gmail.com>
2022-11-24 15:57:08 -0300

    adjusting App virtual tour logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,21 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import { Governance } from "./Pages/Governance";
-import { GeneralNavBar } from "./Components";
-import { Footer } from "./Components";
-import Admission from "./Pages/Admission/Admission";
-import { VirtualTour } from "./Pages/VirtualTour";
-import Home from "./Pages/Home/Home";
-import { useState, useEffect } from "react";
+import { GeneralNavBar, Footer } from "./Components";
+import { VirtualTour, Governance, Admission, Home } from "./Pages";
+
 export default function App() {
-  const [tour, setTour] = useState(false);
-  const location = useLocation()
-  useEffect(()=>{
-    if(location.pathname == '/tour'){
-      setTour(true)
-    } else{
-      setTour(false)
-    }
-  }, [location])
+  const { pathname } = useLocation();
+  const isVirtualTour = pathname === "/tour";
+
   return (
     <div className="App">
-      {!tour ? (
-          <>
-          <GeneralNavBar></GeneralNavBar>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home/admissão" element={<Admission />} />
-              <Route path="/home/governança" element={<Governance />} />
-              <Route
-                path="/tour"
-                element={<VirtualTour />}
-              />
-            </Routes>
-          <Footer></Footer>
-          </>
-      ) : (
-            <>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route
-                path="/tour"
-                element={<VirtualTour />}
-              />
-            </Routes>
-            </>
-      )}
-      </div>
+      <GeneralNavBar style={{ display: isVirtualTour ? "none" : "block" }} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home/admissão" element={<Admission />} />
+        <Route path="/home/governança" element={<Governance />} />
+        <Route path="/tour" element={<VirtualTour />} />
+      </Routes>
+      <Footer style={{ display: isVirtualTour ? "none" : "block" }} />
+    </div>
   );
 }
