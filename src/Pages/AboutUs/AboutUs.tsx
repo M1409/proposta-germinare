498f8add49cfc8e19c126ed367e72b409bfbd0ca
AlexandroMinobe <alexkatsuharu@gmail.com>
2022-11-28 23:50:34 -0300

    Values initial integration with description

--- src/Pages/AboutUs/AboutUs.tsx
+++ src/Pages/AboutUs/AboutUs.tsx
@@ -1,13 +1,15 @@
 import { Values } from './Values'
 import './style.scss'
 import $ from 'jquery'
+import { ContentSpacer } from '../../Components'
+
+
 
 export function AboutUs(){
     return(
-        <>
-
+        <ContentSpacer>
             <Values/>
-            
-        </>
+
+        </ContentSpacer>
     )
 }
--- src/Pages/AboutUs/Values/Values.jsx
+++ src/Pages/AboutUs/Values/Values.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export function ConnectDots({ cx, cy, fill, brightness = 100 }) {
   return (
     <circle
@@ -11,16 +13,20 @@ export function ConnectDots({ cx, cy, fill, brightness = 100 }) {
 }
 
 export function ValueConfig({
-  start,
-  end,
-  curve1,
-  curve2,
-  curve3,
-  curve4,
-  curve5,
+  coords: {
+    start,
+    end,
+    curve1,
+    curve2,
+    curve3,
+    curve4,
+    curve5,
+  },
   fill,
-  label,
+  name,
+  description,
   x = 30,
+  setCurrentValue
 }) {
   const id = Math.random().toString();
   const relativeD = ` M ${start.cx} ${start.cy} C ${curve1.cx} ${curve1.cy} ${curve2.cx} ${curve2.cy} ${end.cx} ${end.cy} Q ${curve3.cx} ${curve3.cy} ${start.cx} ${start.cy} Z`;
@@ -30,12 +36,12 @@ export function ValueConfig({
     <g>
       <path d={relativeD} fill={fill} />
       <path d={relativeDText} fill="none" id={id} />
-      <text x={`${x}px`}>
+      <text x={`${x}px`} onClick={()=>{setCurrentValue({name:name, description: description})}}>
         <textPath
           xlinkHref={`#${id}`}
           style={{ fill: "white", fontSize: "13.5px" }}
         >
-          {label}
+          {name}
         </textPath>
       </text>
     </g>
@@ -91,42 +97,49 @@ let values = [
     fill: "#1585B4",
     coords: {},
     x: 35,
+    description: "Comprometido com o resultado, conhece com profundidade aquilo que faz e tem a visão do todo. Age com obstinação, disciplina e foco no detalhe. É mão na massa, busca sempre ser o melhor naquilo que faz e não desiste nunca. Está sempre disponível e dá o exemplo. Indigna-se, não se conforma, não fica quieto nem se omite quando vê algo que não funciona bem ou possa ser melhorado. É atento aos gastos e à economia de cada centavo. Está engajado com a cultura da organização."
   },
   {
     name: "Determinação",
     fill: "#79AB5C",
     coords: {},
     x: 45,
+    description: "É obstinado, entrega resultados superiores e cumpre seus compromissos. Faz as coisas acontecerem, busca alternativas para os problemas e engaja as pessoas em prol de um objetivo comum. Tem senso de urgência, atitude de dono e não desiste nunca."
   },
   {
     name: "Disciplina",
     fill: "#EF933B",
     coords: {},
     x: 55,
+    description: "Cumpre o combinado, é pontual com horário e seus compromissos. Executa suas tarefas de forma disciplinada. É focado, pragmático, otimiza o tempo, atividades e recursos. Entrega resultados, não cria justificativas e desculpas."
   },
   {
     name: "Disponibilidade",
     fill: "#E86339",
     coords: {},
     x: 40,
+    description: "É receptivo, acessível, disponível, não tem dia e não tem hora, está sempre pronto e tem o trabalho como prioridade. Está aberto ao novo, às mudanças e motivado para novos desafios."
   },
   {
     name: "Simplicidade",
     fill: "#46A261",
     coords: {},
     x: 48,
+    description: "Faz as coisas acontecerem de forma simples e prática, é mão na massa, vai direto ao ponto, descomplica e desburocratiza respeitando as normas."
   },
   {
     name: "Franqueza",
     fill: "#D5202A",
     coords: {},
     x: 50,
+    description: "É direto, sincero, verdadeiro e transparente em suas relações, sempre com respeito, de forma positiva, agregadora e acolhedora. Não se omite, expressa suas opiniões mesmo quando contrária aos demais. Sabe dizer não."
   },
   {
     name: "Humildade",
     fill: "#1B9BC7",
     coords: {},
     x: 55,
+    description: "Sabe ouvir, é atencioso, considera a opinião dos outros, não importa quem fez, importa que fizemos. Não tem vergonha de perguntar nem de dizer que não sabe. Não é arrogante nem vaidoso e age com respeito. Não se preocupa com status nem se acha dono da verdade. Prioriza o nós e não o eu."
   },
 ];
 
@@ -158,17 +171,20 @@ convertPoints(polygon([200, 200], 7, 150, 1.6)).forEach((it, index) => {
   values[index].coords["curve5"] = it;
 });
 
-export function Values() {
-  
 
+
+export function Values() {
+  const [currentValue, setCurrentValue] = useState({
+    name: "Nossos valores",
+    description: ""
+  })
   return (
+    <div style={{display: "flex", margin: "0 auto", justifyContent: "space-around"}}>
       <svg width="400px" height="400px" >
         {values.map((it) => (
           <ValueConfig
-            {...it.coords}
-            fill={it.fill}
-            label={it.name}
-            x={it?.x}
+            {...it}
+            setCurrentValue={setCurrentValue}
           />
         ))}
         <text  fontSize="20px">
@@ -176,5 +192,10 @@ export function Values() {
           <tspan  x="160" y="220">um valor</tspan>
         </text>
       </svg>
+      <div style={{display: "flex", flexDirection: "column", width: "400px"}}>
+          <h1>{currentValue.name}</h1>
+          <p>{currentValue.description}</p>
+      </div>
+    </div>
   );
 }
